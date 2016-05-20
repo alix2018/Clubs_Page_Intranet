03887ef0c151226ea50ac8397ed18d7d4b4cbd99
Morineau Charlelie <charlelie.morineau@hotmail.fr>
2016-05-20 04:04:58 +0200

    Ajout sécurité formulaire event

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,8 @@ class Event < ActiveRecord::Base
   validates :date_end, presence: {message: "Tu trouves pas qu't'as oublié quelque chose?!"}
   validate :start_must_be_before_end_date
   validates :title, length: { maximum: 140 }
+  validate :start_must_be_before_actual_time
+  validate :end_must_be_before_actual_time
 
   belongs_to :club
   belongs_to :user
@@ -17,5 +19,18 @@ class Event < ActiveRecord::Base
     end
   end
 
+  #Permet d'éviter à un membre de créer un événement antérieure à la date et heure actuelle
+  def start_must_be_before_actual_time
+    if date_start <= Time.now.strftime("%Y-%m-%d %H:%M")
+      errors.add(:date_start, "Tu as saisi une date antérieure à la date actuelle") unless
+        date_start >= Time.now.strftime("%Y-%m-%d %H:%M")
+    end
+  end
 
+  def end_must_be_before_actual_time
+    if date_end <= Time.now.strftime("%Y-%m-%d %H:%M")
+      errors.add(:date_end, "Tu as saisi une date antérieure à la date actuelle") unless
+        date_end >= Time.now.strftime("%Y-%m-%d %H:%M")
+    end
+  end
 end
